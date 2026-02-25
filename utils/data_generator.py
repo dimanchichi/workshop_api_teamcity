2874733492dbf5110e811610b456a8e7e32efa66
dimanchichi <dimanchichi@yahoo.com>
2026-02-26 00:16:45 +0300

    fix

--- utils/data_generator.py
+++ utils/data_generator.py
@@ -10,7 +10,7 @@ class DataGenerator:
     @staticmethod
     def generate_project_id():
         first_latter = faker_instance.random.choice(string.ascii_letters)
-        rest_characters = ''.join(faker_instance.random.choises(string.ascii_letters + string.digits, k=10))
+        rest_characters = ''.join(faker_instance.random.choices(string.ascii_letters + string.digits, k=10))
         project_id = first_latter + rest_characters
         return project_id
 
