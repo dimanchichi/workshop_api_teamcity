659a19f895923f708ac75532818906a36e638602
dimanchichi <dimanchichi@yahoo.com>
2026-02-28 18:02:22 +0300

    create project api

---
+++ api/project_api.py
@@ -0,0 +1,30 @@
+from http import HTTPStatus
+
+from custom_requester.requester import CustomRequester
+
+
+class ProjectAPI(CustomRequester):
+    """
+    Класс для работы с проектом
+    """
+    def __init__(self, session):
+        super().__init__()
+        self.session = session
+
+    def create_project(self, project_data):
+        return self.send_request("POST", "/app/rest/projects", data=project_data)
+
+    def get_project(self):
+        return self.send_request("GET", f"/app/rest/projects/")
+
+    def delete_project(self, project_id, expected_status=HTTPStatus.NO_CONTENT):
+        return self.send_request("DELETE", f"/app/rest/projects/id:{project_id}", expected_status=expected_status)
+
+    def clean_up_project(self, project_id):
+        """
+        Метод для проверки полного удаления проекта
+        """
+        self.delete_project(project_id)
+        get_project_response = self.get_project().json()
+        all_projects_ids = [project.get("id", {}) for project in get_project_response.get("project", [])]
+        assert project_id not in all_projects_ids, "ID удаленного проекта найден в списке существующих проектов"
