48857cfbe5baeac63452032c179af696b285c657
dimanchichi <dimanchichi@yahoo.com>
2026-02-28 14:10:43 +0300

    logging

--- custom_requester/requester.py
+++ custom_requester/requester.py
@@ -1,3 +1,5 @@
+import logging
+import os
 from http import HTTPStatus
 from typing import Optional, Any, Dict
 
@@ -12,11 +14,13 @@ class CustomRequester:
     def __init__(self, session):
         self.session = session
         self.base_url = BASE_URL
+        self.logger = logging.getLogger(__name__)
 
     def send_request(self, method: str,
                      endpoint: str,
                      data: Optional[Dict[str, Any]] = None,
-                     expected_status: HTTPStatus = HTTPStatus.OK):
+                     expected_status: HTTPStatus = HTTPStatus.OK,
+                     need_logging = True):
         """
         Враппер для запросов
         :param method: метод запроса
@@ -27,6 +31,8 @@ class CustomRequester:
         """
         url = f"{self.base_url}{endpoint}"
         response = self.session.request(method=method.upper(), url=url, json=data)
+        if need_logging:
+            self.log_request_and_response(response)
         if response.status_code != expected_status:
             raise ValueError(f"Unexpected status code: {response.status_code}. Expected: {expected_status.value}")
         return response
@@ -35,6 +41,43 @@ class CustomRequester:
         self.headers=self.base_headers.copy()
         self.headers.update(kwargs)
         self.session.headers.update(self.headers)
+
+    def log_request_and_response(self, response):
+        try:
+            request = response.request
+            GREEN = "\033[92m"
+            RED = "\033[91m"
+            RESET = "\033[0m"
+            headers = "\\\n".join([f"-H '{headers}: {value}'" for headers, value in request.headers.items()])
+            full_test_name = f"pytest {os.environ.get('PYTEST_CURRENT_TEST', '').replace('(call)', '')}"
+
+            body = ""
+            if hasattr(request, "body") and request.body is not None:
+                if isinstance(request.body, bytes):
+                    body = request.body.decode("utf-8")
+            body = f"-d '{body}' \n" if body != '{}' else ""
+
+            self.logger.info(
+                f"{GREEN}{full_test_name}{RESET}\n"
+                f"curl - X {request.method} '{request.url}' \\\n"
+                f"{headers} \\\n"
+                f"{body}"
+            )
+
+            response_status = response.status_code
+            is_success = response.ok
+            response_data = response.text
+
+            if not is_success:
+                self.logger.info(
+                    f"\nRESPONSE:"
+                    f"\nSTATUS_CODE: {RED}{response_status}{RESET}"
+                    f"\nDATA: {RED}{response_data}{RESET}"
+                )
+        except Exception as e:
+            self.logger.info(f"\nLogging went wrong: {type(e)} - {e}")
+
+
 
         
 
