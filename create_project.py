dec1eb541735788ea4a73e5ffa5855df9aa85235
dimanchichi <dimanchichi@yahoo.com>
2026-03-09 01:55:11 +0300

    update

--- create_project.py
+++ create_project.py
@@ -1,13 +1,8 @@
-from http import HTTPStatus
+import time
 
-import requests
-from pprint import pprint
-
-from custom_requester import requester
 from data_for_test.build_config_data import BuildConfigData
 from data_for_test.build_type_data import BuildTypeData
 from data_for_test.project_data import ProjectData
-from custom_requester.requester import CustomRequester
 
 class TestProjectCreate:
 
@@ -18,30 +13,30 @@ class TestProjectCreate:
         cls.build_config_data = BuildConfigData.create_build_config_data(cls.project_id)
         cls.build_config_id = cls.build_config_data["id"]
         cls.build_type_data = BuildTypeData.create_build_type_data(cls.build_config_id)
+        cls.build_type_data_id = cls.build_type_data["buildType"]["id"]
 
-    def test_create_project(self):
-        requester = CustomRequester(requests.Session())
-        requester.session.auth = ("admin", "admin")
+    def test_create_project(self, api_manager):
+        create_project_response = api_manager.project_api.create_project(self.project_data).json()
+        assert create_project_response.get("id") == self.project_id
 
-        csrf_token = requester.send_request("GET", "/authenticationTest.html?csrf").text
-        requester._update_session_headers(**{"X-TC-CSRF-Token": csrf_token})
+        get_project_response = api_manager.project_api.get_project().json()
+        all_projects_ids = [project.get("id", {}) for project in get_project_response.get("project", [])]
+        assert self.project_id in all_projects_ids, "ID проекта не найден в списке существующих проектов"
 
-        requester.send_request("POST", "/app/rest/projects", data=self.project_data)
+        api_manager.build_config_api.create_build_config(self.build_config_data)
 
-        requester.send_request("GET", f"/app/rest/projects/{self.project_id}")
+        get_build_config_response = api_manager.build_config_api.get_build_config_details().json()
+        all_builds_ids = [build_type["id"] for build_type in get_build_config_response["buildType"]]
+        assert self.build_config_id in all_builds_ids
 
-        requester.send_request("POST", "/app/rest/buildTypes", data=self.build_config_data, expected_status=HTTPStatus.INTERNAL_SERVER_ERROR)
+        start_build_response = api_manager.build_config_api.start_build_config(self.build_type_data).json()
 
-        requester.send_request("GET", f"/app/rest/buildTypes/{self.build_config_id}")
+        time.sleep(2)
 
-        started_build_id = requester.send_request("POST", f"/app/rest/buildQueue", data=self.build_type_data).json()["id"]
+        get_started_build_response = api_manager.build_config_api.get_build_type_details().json()
+        all_started_builds_ids = [build["buildTypeId"] for build in get_started_build_response["build"]]
+        assert start_build_response["buildType"]["id"] in all_started_builds_ids
 
-        requester.send_request("GET", f"/app/rest/builds/{started_build_id}")
+        api_manager.build_config_api.clean_up_build_config(self.build_config_id)
 
-        requester.send_request("DELETE", f"/app/rest/buildTypes/{self.build_config_id}", expected_status=HTTPStatus.NO_CONTENT)
-
-        requester.send_request("GET", f"/app/rest/buildTypes/{self.build_config_id}", expected_status=HTTPStatus.NOT_FOUND)
-
-        requester.send_request("DELETE", f"/app/rest/projects/{self.project_id}", expected_status=HTTPStatus.NO_CONTENT)
-
-        requester.send_request("GET", f"/app/rest/projects/{self.project_id}", expected_status=HTTPStatus.NOT_FOUND)
+        api_manager.project_api.clean_up_project(self.project_id)
