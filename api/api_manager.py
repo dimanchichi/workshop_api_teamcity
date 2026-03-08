e53ae3ebcc357c9063fdbb2f894a3d6665d22caf
dimanchichi <dimanchichi@yahoo.com>
2026-03-09 01:56:03 +0300

    update

--- api/api_manager.py
+++ api/api_manager.py
@@ -1,4 +1,5 @@
 from api.auth_api import AuthAPI
+from api.build_api import BuildAPI
 from api.project_api import ProjectAPI
 
 
@@ -7,3 +8,4 @@ class ApiManager:
         self.session = session
         self.auth_api = AuthAPI(session)
         self.project_api = ProjectAPI(session)
+        self.build_config_api = BuildAPI(session)
